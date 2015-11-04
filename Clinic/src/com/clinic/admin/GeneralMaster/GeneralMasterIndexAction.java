e8e6b5bacf68a32d809073c86287772900b1a5ed
Vineet <vin4friends@gmail.com>
2015-11-04 19:52:54 +0530

    Select general type based on request param

--- Clinic/src/com/clinic/admin/GeneralMaster/GeneralMasterIndexAction.java
+++ Clinic/src/com/clinic/admin/GeneralMaster/GeneralMasterIndexAction.java
@@ -6,10 +6,17 @@ package com.clinic.admin.GeneralMaster;
 import java.util.List;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.struts2.ServletActionContext;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.app.common.fe.impl.AbstractListAction;
 import com.clinic.admin.bl.generalMaster.GeneralMasterAdministrationFacade;
 import com.clinic.admin.model.impl.GeneralMaster;
+import com.opensymphony.xwork2.ActionContext;
 
 
 /**
@@ -35,5 +42,31 @@ public class GeneralMasterIndexAction extends AbstractListAction<GeneralMaster> 
 	@Override
 	protected int getPageSizeInternal() {
 		return generalMasterAdministrationFacade.getPageSize();
+	}
+	
+	public List<String> getGeneralMasterTypes(){
+		System.err.println("in get masters");
+		//System.err.println(generalMasterAdministrationFacade.getGeneralMasterTypes());
+		return generalMasterAdministrationFacade.getGeneralMasterTypes();
+	}
+	
+	public List<GeneralMaster> getGeneralMasterByType(){
+		//ServletActionContext.getRequest().getParameter("");
+		HttpServletRequest req = ServletActionContext.getRequest();
+		
+		System.err.println("--------------------------------------------------------" + req.getParameter("MasterType"));
+		System.err.println(super.getText("masterType"));
+		String masterType = req.getParameter("MasterType");
+		
+		if(StringUtils.isNotEmpty(masterType)){
+			return generalMasterAdministrationFacade.findGeneralMasterByType(masterType);
+		}
+		
+		return generalMasterAdministrationFacade.findGeneralMasterByType("RELIGION");
+	}
+	
+	public @ResponseBody String byParameter(@RequestParam("masterType") String foo) {
+	    return "Mapped by path + method + presence of query parameter! (MappingController) - foo = "
+	           + foo;
 	}
 }
